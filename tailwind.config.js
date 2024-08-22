82f506839b41b0825afdbe16db466d250b67a555
vaibhav-372 <133035287+vaibhav-372@users.noreply.github.com>
2024-08-23 01:27:33 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx,html}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'home': "url('/assets/background.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
